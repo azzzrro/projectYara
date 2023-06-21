154a2b6e10e2624a53653aa777df053a4a058f7b
azzzrro <helloazzzrro@gmail.com>
2023-06-21 13:19:12 +0530

    db change

--- config/mongo.js
+++ config/mongo.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = mongoose;
